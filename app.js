4c0b3cd86b26cc635ddf93058dd65c2d225a3af1
Matt Hand <mmhand123@gmail.com>
2016-01-25 17:30:04 -0500

    Added basic video chat example

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -22,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+// app.use('/', routes);
 // app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -31,6 +27,8 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+app.get('/', express.static(__dirname + '/public'));
 
 // error handlers
 
@@ -55,6 +53,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
