c784756919b3542be8142d9a63ea463367413c28
imrokas <rokas@imrokas.com>
2016-01-29 23:35:25 -0500

    (chores) refactored package.json, added gulp, mocha, removed webpack, jest [closes #37, closes #38, closes #39, closes #40, closes #41]

---
+++ src/hello.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/**
+ * @module src/hello
+ */
+
+import util from 'util';
+
+/**
+ * Gets greeting message in format "Hello, %s"
+ *
+ * @param {string} [name=world] Name to insert
+ *
+ * @return {string} Greeting message
+ *
+ * @example
+ *
+ *  import hello from 'src/hello';
+ *
+ *  hello('username'); // 'Hello, username'
+ */
+export default function hello(name) {
+  name = name || 'world';
+
+  return util.format('Hello, %s!', name);
+}
