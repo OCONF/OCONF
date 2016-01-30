8a2311f0c985e03a47e560bdaa2687df53dbf520
Matt Hand <mmhand123@gmail.com>
2016-01-30 03:35:13 -0500

    (cleanup) Cleaned up refactored code

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@
  * Controls the app, imports other functionality
  *
  */
-import $ from 'jquery';
+// import $ from 'jquery';
 import { audioMuteControl, videoMuteControl, screenShare } from './controllers/button-control';
-import { peerJoined, peerLeft, peerUpdated,  } from './controllers/peer-control';
+import { peerJoined, peerLeft, peerUpdated } from './controllers/peer-control';
 
 export const Skynet = new window.Skylink();
-(function App () {
+(function App() {
   // On load, initialize new Skylink connection
   Skynet.setDebugMode({ storeLogs: true });
 
@@ -31,25 +31,25 @@ export const Skynet = new window.Skylink();
   // TODO: Init may need to be refactored to support different rooms, or at least re-called
   Skynet.on('mediaAccessSuccess', stream => {
     const vid = document.getElementById('myvideo');
-    attachMediaStream(vid, stream);
+    window.attachMediaStream(vid, stream);
   });
 
  // Handle that oncoming stream, filter it into new vid elements (made by peer functions)
   Skynet.on('incomingStream', (peerId, stream, isSelf) => {
     if (isSelf) return;
-    let vid = document.getElementById(`${peerId}`);
-    attachMediaStream(vid, stream);
+    const vid = document.getElementById(`${peerId}`);
+    window.attachMediaStream(vid, stream);
   });
 
   Skynet.init({
     // Localhost testing key only for now
     apiKey: '44759962-822a-42db-9de2-39a31bf25675',
     // Yas
-    defaultRoom: 'test'
+    defaultRoom: 'test',
   }, () => {
     Skynet.joinRoom({
       audio: true,
-      video: true
+      video: true,
     });
   });
-})();
+}());
