98f591f1214c0c61f0ae202017cf4e1ba540e411
Matt Hand <mmhand123@gmail.com>
2016-02-02 14:35:41 -0500

    (fix) Fixed username prompt coming up twice [closes #77]

--- src/controllers/room-control.js
+++ src/controllers/room-control.js
@@ -1,10 +1,12 @@
 import randomWord from 'random-words';
+import { Skynet } from '../index';
 
 export default function chooseRoom() {
   const path = window.location.pathname.slice(5);
-
+  console.log(path);
   if (path) {
     window.room = path;
+    getUserName();
   } else {
     window.room = getRandomRoom();
     window.location = `/app/${window.room}`;
@@ -15,4 +17,11 @@ export default function chooseRoom() {
 function getRandomRoom() {
   let randomWords = randomWord({exactly: 4});
   return randomWords.join('');
+}
+
+function getUserName() {
+  window.user = prompt('What is your name?');
+  Skynet.setUserData({
+    displayName: window.user,
+  });
 }
